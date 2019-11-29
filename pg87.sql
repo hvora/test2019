98d8467da5f74bcb61542e19e0ca6603125703e6
hvora <hita.courses@gmail.com>
2019-11-29 01:34:02 -0800

    Add files via upload
    testing  how to upload files

---
+++ pg87.sql
@@ -0,0 +1,9 @@
+SELECT
+	ipps_2012.*, hospital_general_information.hospital_name, hospital_general_information.state
+FROM
+	ipps_2012
+JOIN
+	hospital_general_information ON ipps_2012.provider_id =
+    hospital_general_information.provider_id;
+    
+    
